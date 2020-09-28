47bfb1aeb4f5fde89a9838081934e138270df353
Ainesh Sootha <34103205+AineshSootha@users.noreply.github.com>
2020-09-28 18:47:56 -0400

    Add files via upload

--- producer.py
+++ producer.py
@@ -34,37 +34,23 @@ schema = avro.schema.parse(json.dumps({
         {"name" : "comments", "type": "string"}
     ]
 }))
-reader_schema = avro.schema.parse(json.dumps({
-    "namespace": "customers.avro",
-    "type": "record",
-    "name": "pythonTest",
-    "fields": [
-        {"name" : "id", "type": "string"},
-        {"name" : "first_name", "type": "string"},
-        {"name" : "last_name", "type": "string"},
-        {"name" : "email", "type": "string"},
-        {"name" : "gender", "type": "string"},
-        {"name" : "club_status", "type": "string"},
-        {"name" : "comments", "type": "string"}
-    ]
-}))
 
+# reader_schema = avro.schema.parse(json.dumps({
+#     "namespace": "customers.avro",
+#     "type": "record",
+#     "name": "pythonTest",
+#     "fields": [
+#         {"name" : "id", "type": "string"},
+#         {"name" : "first_name", "type": "string"},
+#         {"name" : "last_name", "type": "string"},
+#         {"name" : "email", "type": "string"},
+#         {"name" : "gender", "type": "string"},
+#         {"name" : "club_status", "type": "string"},
+#         {"name" : "comments", "type": "string"}
+#     ]
+# }))
 
-# avroProducer = Producer({
-#     'bootstrap.servers': 'kafka:9092',
-#     #'on_delivery': delivery_report
-#     #'schema.registry.url': 'http://schema_registry_host:port'
-#     }#, default_key_schema=key_schema, default_value_schema=value_schema)
-
-# for i in reader:
-#     avroProducer.produce(topic='test_topic', value = dict(i))
-
-# avroProducer.flush()
-#producer = SimpleProducer(client)
-#topic = "topic_test"
 writer = DataFileWriter(open("transactions.avro", "wb"), DatumWriter(), schema)
-
-#writer.append({"id": "1000", "first_name": "Ainesh", "last_name": "Sootha", "email": "ainesh", "gender": "M", "club_status": "as","comments": "nao"})
 
 producer = KafkaProducer(
     bootstrap_servers='kafka:9093',
@@ -77,7 +63,6 @@ producer = KafkaProducer(
 #number_of_rows = cursor.execute("select * from trans_produced")
 
 
-
 #result = {}
 #for row in reader:
 #     key = row[0]
@@ -85,45 +70,29 @@ producer = KafkaProducer(
 # #result = cursor.fetchall()
 
 for i in reader:
-    #writer = DatumWriter(schema)
     bytes_writer = io.BytesIO()
     encoder = avro.io.BinaryEncoder(bytes_writer)
     writer.append(dict(i))
-    #producer.send("topic_test", raw_bytes)
-    #print(i)
 
-# #     sleep(0.5)
+
 
 writer.close()
+
 reader = DataFileReader(open("transactions.avro", "rb"), DatumReader(schema, reader_schema))
-#new_schema = reader.get_meta("avro.schema")
+
 users = []
 for user in reader:
     users.append(user)
 
-#producer.send('topic_test', {1:'hello'})
 
 for i in users:
-    print(i)
+    # print(i)
     producer.send('topic_test', value=i)
     sleep(0.2)
-    
-# def on_send_success(record_metadata):
-#     print(record_metadata.topic)
-#     print(record_metadata.partition)
-#     print(record_metadata.offset)
-# def on_send_error(excp):
-#     logging.error('I am an errback', exc_info=excp)
 
-
-#print(producer.send('test_topic', b'some_message_bytes').get(timeout=30))
 
 
 producer.flush()
-
-
-
-#producer.close()
 
 reader.close()
 #db.close()
